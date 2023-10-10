acf4358956a1ada4134c778a89998782e78aa504
rosalub <rosalie.dauchez.etu@univ-lille.fr>
2023-10-10 11:58:36 +0200

    fix: make the test works with the exceptions errors

--- moving_to_poetry/common_prefix.py
+++ moving_to_poetry/common_prefix.py
@@ -1,4 +1,4 @@
-def longestCommonPrefix(strs: list[str]) -> str:
+def longestCommonPrefix(strs: list) -> str:
         """
         Find the longest common prefix string among an array of strings
         
@@ -10,16 +10,16 @@ def longestCommonPrefix(strs: list[str]) -> str:
         """
         if len(strs) == 0 :
              return None 
+        for element in strs :
+            if element == '' :
+                raise IndexError("Please remove the empty string from the list")
         n = min(map(lambda x : len(x), strs))
         index = 0
         prefix = ""
         for index in range(n) :
             prefix_temp = set()
-            for element in strs : 
-                try : 
+            for element in strs :
                     prefix_temp.add(element[index])
-                except IndexError :
-                     print("Please remove the empty string from the list")
             if len(prefix_temp) != 1 :
                 return prefix 
             prefix += ''.join(prefix_temp)
--- tests/test_common_prefix.py
+++ tests/test_common_prefix.py
@@ -1,10 +1,13 @@
+import pytest
 from moving_to_poetry.common_prefix import longestCommonPrefix
 
-def test_longest_common_prefix(self):
-    strs = ["flower","flow","flight"]
-    strs_bis = ["flower","flow","flight", '']
-    pref = longestCommonPrefix(strs)
+strs = ["flower","flow","flight"]
+strs_bis = ["flower","flow","flight", '']
+
+pref = longestCommonPrefix(strs)
+
+def test_longest_common_prefix():
     assert pref == "fl"
-    with self.assertRaises(IndexError):
+    with pytest.raises(IndexError):
         longestCommonPrefix(strs_bis)
             
