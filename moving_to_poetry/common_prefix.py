86dc0b2f92b25e0eb0f958c340506a2b854c4050
rosalub <rosalie.dauchez.etu@univ-lille.fr>
2023-10-19 14:38:19 +0200

    fix: followed the recommandations to write standard code

--- moving_to_poetry/common_prefix.py
+++ moving_to_poetry/common_prefix.py
@@ -1,26 +1,28 @@
-def longestCommonPrefix(strs: list) -> str:
-        """
-        Find the longest common prefix string among an array of strings
+"""Module docstring (no module needed)"""
+
+def longest_common_prefix(strs: list) -> str:
+    """
+    Find the longest common prefix string among an array of strings
         
-        Parameters :
-        - strs : array of strings (list)
+    Parameters :
+    - strs : array of strings (list)
         
-        Return 
-        - prefix : longest common prefix (str)
-        """
-        if len(strs) == 0 :
-             return None 
+    Return 
+    - prefix : longest common prefix (str)
+    """
+    if len(strs) == 0 :
+        return None
+    for element in strs :
+        if element == '' :
+            raise IndexError("Please remove the empty string from the list")
+    n = min([len(x) for x in strs])
+    index = 0
+    prefix = ""
+    for index in range(n) :
+        prefix_temp = set()
         for element in strs :
-            if element == '' :
-                raise IndexError("Please remove the empty string from the list")
-        n = min(map(lambda x : len(x), strs))
-        index = 0
-        prefix = ""
-        for index in range(n) :
-            prefix_temp = set()
-            for element in strs :
-                    prefix_temp.add(element[index])
+            prefix_temp.add(element[index])
             if len(prefix_temp) != 1 :
-                return prefix 
-            prefix += ''.join(prefix_temp)
-        return prefix
+                return prefix
+        prefix += ''.join(prefix_temp)
+    return prefix
--- tests/test_common_prefix.py
+++ tests/test_common_prefix.py
@@ -1,13 +1,18 @@
+"""Import pytest module to automate the tests of my 
+function"""
+
 import pytest
-from moving_to_poetry.common_prefix import longestCommonPrefix
+from moving_to_poetry.common_prefix import longest_common_prefix
 
 strs = ["flower","flow","flight"]
 strs_bis = ["flower","flow","flight", '']
 
-pref = longestCommonPrefix(strs)
+pref = longest_common_prefix(strs)
 
 def test_longest_common_prefix():
+    """tests the limits of the 
+    longest_common_previx function"""
     assert pref == "fl"
     with pytest.raises(IndexError):
-        longestCommonPrefix(strs_bis)
+        longest_common_prefix(strs_bis)
             
