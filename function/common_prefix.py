88a4e03b63226ccb7a2ffe6c013a7c905d3d5a83
rosalub <rosalie.dauchez.etu@univ-lille.fr>
2023-10-27 11:01:19 +0200

    fix: followed the recommandations to write standard code

--- function/common_prefix.py
+++ function/common_prefix.py
@@ -17,10 +17,10 @@ def longest_common_prefix(strs: list) -> str:
         if element == '' :
             raise IndexError("Please remove the empty string from the list")
         lenghts.append(len(element))
-    n = min(lenghts)
+    max_lenght = min(lenghts)
     index = 0
     prefix = ""
-    for index in range(n) :
+    for index in range(max_lenght) :
         prefix_temp = set()
         for element in strs :
             prefix_temp.add(element[index])
